ad2d3c4bfa23c85bc94b11da9cb8f98d8bb7ae6d
Fekete Balazs <48908633+fbv81bp@users.noreply.github.com>
2021-11-25 20:30:40 +0100

    Add files via upload

---
+++ sine.py
@@ -0,0 +1,25 @@
+import math
+
+step = [0 for i in range(1000)]
+step[100] = 1 # needs a Dirac impulse to start swinging
+
+amp = 0.995 # exponentially increasing amplitude
+#amp = 1     # constant amplitude
+#amp = 1.005 # exponentially decreasing amplitude
+freq = 137 # a lovely frequency for a 1000 long array
+
+sine = [0,0] # initialized to ease indexing
+
+for i in range(1,len(step)-2):
+    sine.append((amp*math.sin(2*3.14159*freq)*step[i] - sine[i-1] + 2*amp*math.cos(2*3.141459*freq)*sine[i]) / amp**2)
+
+
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+plt.title("Sine with Z-transform")
+plt.plot(sine, color="blue")
+
+plt.show()
