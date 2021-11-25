a0608acc8235a9a15964751cd42cf0d61ef1f6c5
Fekete Balazs <48908633+fbv81bp@users.noreply.github.com>
2021-11-25 20:38:28 +0100

    Fixed comments' ident

--- sine.py
+++ sine.py
@@ -1,12 +1,12 @@
 import math
 
 dirac = [0 for i in range(1000)]
-dirac[100] = 1 # needs a Dirac impulse to start swinging
+dirac[100] = 1  # needs a Dirac impulse to start swinging
 
-amp = 0.995 # exponentially increasing amplitude
-#amp = 1     # constant amplitude
-#amp = 1.005 # exponentially decreasing amplitude
-freq = 137 # a lovely frequency for a 1000 long array
+amp = 0.995     # exponentially increasing amplitude
+#amp = 1        # constant amplitude
+#amp = 1.005    # exponentially decreasing amplitude
+freq = 137      # a lovely frequency for a 1000 long array
 
 sine = [0,0] # initialized to ease indexing
 
