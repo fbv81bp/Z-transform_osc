4480637fa00f4cbf7cd81b158e22925e24838f12
Fekete Balazs <48908633+fbv81bp@users.noreply.github.com>
2021-11-25 20:37:59 +0100

    Renamed a variable

--- sine.py
+++ sine.py
@@ -1,17 +1,17 @@
 import math
 
-step = [0 for i in range(1000)]
-step[100] = 1   # needs a Dirac impulse to start swinging
+dirac = [0 for i in range(1000)]
+dirac[100] = 1 # needs a Dirac impulse to start swinging
 
-amp = 0.995     # exponentially increasing amplitude
-#amp = 1        # constant amplitude
-#amp = 1.005    # exponentially decreasing amplitude
-freq = 137      # a lovely frequency for a 1000 long array
+amp = 0.995 # exponentially increasing amplitude
+#amp = 1     # constant amplitude
+#amp = 1.005 # exponentially decreasing amplitude
+freq = 137 # a lovely frequency for a 1000 long array
 
-sine = [0,0]    # initialized to ease indexing
+sine = [0,0] # initialized to ease indexing
 
-for i in range(1,len(step)-2):
-    sine.append((amp*math.sin(2*3.14159*freq)*step[i] - sine[i-1] + 2*amp*math.cos(2*3.141459*freq)*sine[i]) / amp**2)
+for i in range(1,len(dirac)-2):
+    sine.append((amp*math.sin(2*3.14159*freq)*dirac[i] - sine[i-1] + 2*amp*math.cos(2*3.141459*freq)*sine[i]) / amp**2)
 
 
 import matplotlib.pyplot as plt
